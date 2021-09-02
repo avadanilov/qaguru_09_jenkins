735df3e0d138d728907b256fecb9eeb2ff538026
Aleksandr Danilov <avdanilov@mts.ru>
2021-09-02 15:42:23 +0300

    add url and credential parameters

---
+++ src/test/java/guru/qa/qaguru_09_jenkins/config/Credentials.java
@@ -0,0 +1,7 @@
+package guru.qa.qaguru_09_jenkins.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Credentials {
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+}
